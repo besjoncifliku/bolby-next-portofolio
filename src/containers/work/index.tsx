4d1286068d9748ad9601fac8fd3b88e3579fbd08
Besjon <bcifliku17@epoka.edu.al>
2024-03-08 20:06:48 +0100

    Initial Setup for the exapndible form and style fixes

--- src/containers/work/index.tsx
+++ src/containers/work/index.tsx
@@ -88,10 +88,10 @@ export const WorkView = () => {
                     ))}
 
                     <div className={'search-bar absolute right-7 w-5/12'}>
-                        <label htmlFor="simple-search" className="sr-only">Search</label>
+                        <label htmlFor="simple-search">Search</label>
                         <div className="relative w-full">
                             <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                                <PlaylistAddCheckIcon className={"w-4 h-4 text-gray-500 dark:text-gray-400"} />
+                                <PlaylistAddCheckIcon className={"w-8 h-8 text-gray-500 dark:text-gray-400"} />
                             </div>
                             <input type="text" id="simple-search"
                                    className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
